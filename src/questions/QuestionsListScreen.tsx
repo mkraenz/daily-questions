0277a266d95be2da4986d27de6440f5abca6a870
Mirco Kraenz <kraenzmirco@gmail.com>
2022-06-22 20:41:55 +0200

    feat(questions): questions order via long press and drag-and-drop

--- src/questions/QuestionsListScreen.tsx
+++ src/questions/QuestionsListScreen.tsx
@@ -1,51 +1,73 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { FC } from "react";
-import { ScrollView, View } from "react-native";
+import {
+  NestableDraggableFlatList,
+  NestableScrollContainer,
+  RenderItemParams,
+  ScaleDecorator,
+} from "react-native-draggable-flatlist";
 import { Button, List } from "react-native-paper";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../store";
 import { QuestionsNavigationProp } from "./questions-nav";
-import { Question } from "./questions.slice";
+import { moveQuestion, Question } from "./questions.slice";
 
-const ListItem: FC<Question> = ({ title, questionLong, type, id, active }) => {
+const ListItem: FC<RenderItemParams<Question>> = ({ item, drag, isActive }) => {
+  const { title, questionLong, type, id, active } = item;
   const icon = type === "points" ? "numeric" : "format-color-text";
   const nav = useNavigation<QuestionsNavigationProp>();
   const gotoEditQuestion = () => {
     nav.push("Edit Question", { questionLong, type, id, title, active });
   };
   return (
-    <List.Item
-      onPress={gotoEditQuestion}
-      title={title}
-      description={questionLong}
-      left={(props) => <List.Icon {...props} icon={icon} />}
-      // right={(props) => <List.Icon {...props} icon={"reorder-horizontal"} />}
-    />
+    <ScaleDecorator>
+      <List.Item
+        onPress={gotoEditQuestion}
+        title={title}
+        description={questionLong}
+        left={(props) => <List.Icon {...props} icon={icon} />}
+        onLongPress={drag}
+        disabled={isActive}
+      />
+    </ScaleDecorator>
   );
 };
 
 const mapState = (state: RootState) => ({
   questions: state.questions.questions,
 });
-const connector = connect(mapState);
+const mapDispatch = {
+  moveQuestion,
+};
+const connector = connect(mapState, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const QuestionsListScreen: FC<PropsFromRedux> = ({ questions }) => {
+const QuestionsListScreen: FC<PropsFromRedux> = ({
+  questions,
+  moveQuestion,
+}) => {
   const activeQuestions = questions.filter((q) => q.active); // TODO read selector docs https://redux.js.org/usage/deriving-data-selectors
   const nav = useNavigation<QuestionsNavigationProp>();
   const gotoNewQuestion = () => nav.push("Add new question");
 
   return (
-    <ScrollView>
-      <View style={{ paddingVertical: 20 }}>
-        <Button onPress={gotoNewQuestion} mode="contained">
-          Add new question
-        </Button>
-        {activeQuestions.map((q) => (
-          <ListItem {...q} key={q.id} />
-        ))}
-      </View>
-    </ScrollView>
+    <NestableScrollContainer style={{ marginVertical: 20 }}>
+      <Button
+        onPress={gotoNewQuestion}
+        mode="contained"
+        style={{ marginVertical: 20 }}
+      >
+        Add new question
+      </Button>
+      <NestableDraggableFlatList
+        data={activeQuestions}
+        renderItem={ListItem}
+        keyExtractor={(q) => q.id}
+        onDragEnd={({ to, data }) => {
+          moveQuestion({ to, id: data[to].id });
+        }}
+      />
+    </NestableScrollContainer>
   );
 };
 
--- src/questions/questions.slice.ts
+++ src/questions/questions.slice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { partition } from "lodash";
 import { defaultQuestions } from "./default-questions";
 
 export interface Question {
@@ -19,7 +20,15 @@ const questionsSlice = createSlice({
   initialState,
   reducers: {
     addQuestion(state, action: PayloadAction<Question>) {
-      state.questions.push(action.payload);
+      const [activeQuestions, inactiveQuestions] = partition(
+        state.questions,
+        (q) => q.active
+      );
+      state.questions = [
+        ...activeQuestions,
+        action.payload,
+        ...inactiveQuestions,
+      ];
     },
     editQuestion(state, action: PayloadAction<Question>) {
       const index = state.questions.findIndex(
@@ -28,12 +37,36 @@ const questionsSlice = createSlice({
       state.questions[index] = action.payload;
     },
     archiveQuestion(state, action: PayloadAction<{ id: string }>) {
-      const question = state.questions.find((q) => q.id === action.payload.id)!;
+      const index = state.questions.findIndex(
+        (q) => q.id === action.payload.id
+      )!;
+      const question = state.questions[index];
       question.active = false;
+      // moveQuestion() assumes: archived questions get moved to the end of the questions list
+      state.questions.splice(index, 1);
+      state.questions.push(question);
+    },
+    moveQuestion(state, action: PayloadAction<{ to: number; id: string }>) {
+      // assuming archival moves archived questions to the end of the questions list
+      const { to, id } = action.payload;
+      const movedQuestionIndex = state.questions.findIndex((q) => q.id === id);
+      const movedQuestion = state.questions[movedQuestionIndex];
+
+      const questionsWithoutMovedQuestion = state.questions.filter(
+        (q) => q.id !== id
+      );
+      const questionsBeforeTo = questionsWithoutMovedQuestion.slice(0, to);
+      const questionsAfterTo = questionsWithoutMovedQuestion.slice(to);
+      const questions = [
+        ...questionsBeforeTo,
+        movedQuestion,
+        ...questionsAfterTo,
+      ];
+      state.questions = questions;
     },
   },
 });
 
-export const { addQuestion, editQuestion, archiveQuestion } =
+export const { addQuestion, editQuestion, archiveQuestion, moveQuestion } =
   questionsSlice.actions;
 export default questionsSlice.reducer;
