ff82cae1c70962c7aecf3508a64ffac9a9d545c9
Mirco Kraenz <kraenzmirco@gmail.com>
2022-06-28 20:02:58 +0200

    perf(dailies): memoized answers list

---
+++ src/dailies/dailies.selectors.ts
@@ -0,0 +1,12 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { isInteger } from "lodash";
+import type { RootState } from "../store";
+
+const selectAnswers = (state: RootState) => state.dailies.answers;
+
+export const selectAnswerList = createSelector([selectAnswers], (answers) =>
+  answers
+    .map((a) => a.answer)
+    .filter(isInteger)
+    .join(" ")
+);
