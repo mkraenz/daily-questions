c9527078da3e621872ff063856434224fc649049
Mirco Kraenz <kraenzmirco@gmail.com>
2022-07-11 21:58:17 +0200

    fix(settings): Rendered more hooks than during the previous render when opening settings via drawer menu

--- src/BaseAppBar.tsx
+++ src/BaseAppBar.tsx
@@ -1,16 +1,13 @@
 import { DrawerHeaderProps } from "@react-navigation/drawer";
 import React, { FC } from "react";
 import { Appbar } from "react-native-paper";
-import { useTranslation } from "./localization/useTranslations";
 
 const BaseAppBar: FC<DrawerHeaderProps> = (props) => {
-  const { t } = useTranslation();
   return (
     <Appbar.Header>
       <Appbar.Action
         icon="menu"
         onPress={() => props.navigation.openDrawer()}
-        accessibilityLabel={t("general:openDrawerA11yLabel")}
       />
       <Appbar.Content title={props.options.title ?? props.route.name} />
       {props.children}
