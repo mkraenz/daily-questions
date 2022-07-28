4091ff298e42042f617da8b9332f2b8829052890
Mirco Kraenz <kraenzmirco@gmail.com>
2022-07-28 17:52:08 +0200

    chore(general): v1.20.1 android v21

--- src/about/VersionAndCopyright.tsx
+++ src/about/VersionAndCopyright.tsx
@@ -21,7 +21,7 @@ const VersionAndCopyright = () => {
   const nbsp = "\u00a0";
   return (
     <View style={styles.container}>
-      <Paragraph>Daily Questions v1.20.0</Paragraph>
+      <Paragraph>Daily Questions v1.20.1</Paragraph>
       <TouchableRipple
         onPress={openCompanyWebsite}
         accessibilityRole="link"
