aa13486b8d3137ff972fbc88294a07ba2169b3e2
Mirco Kraenz <kraenzmirco@gmail.com>
2022-09-28 11:23:33 +0200

    fix: fix min height in answer-question screens

--- src/dailies/FulltextQuestionScreen.tsx
+++ src/dailies/FulltextQuestionScreen.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "space-evenly",
     alignItems: "center",
+    minHeight: 280,
   },
   title: {
     fontSize: 32,
--- src/dailies/PointsQuestionScreen.tsx
+++ src/dailies/PointsQuestionScreen.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     paddingRight: 16,
     justifyContent: "space-evenly",
     alignItems: "center",
+    minHeight: 280,
   },
   title: {
     fontSize: 32,
